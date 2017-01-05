fd6c75522f372867249c6a1a4f65930aafb8b5b5
DILEEP KUMAR PANDIYA <DILEEP.PANDIYA01@ad.Infosys.com>
2017-01-05 09:20:40 -0600

    chagne'

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var upload = multer({dest: "./uploads"});
 var mongoose = require("mongoose");
 var path = require('path');
 
-mongoose.connect("mongodb://52.176.48.248/Planogram");
+mongoose.connect("mongo:27017/Planogram");
 var conn = mongoose.connection;
 
 var gfs;
@@ -32,15 +32,19 @@ conn.once("open", function(){
   //second parameter is multer middleware.
   app.post("/", upload.single("avatar"), function(req, res, next){
     //create a gridfs-stream into which we pipe multer's temporary file saved in uploads. After which we delete multer's temp file.
-    console.log(req);
     console.log(req.file.originalname);
     var writestream = gfs.createWriteStream({
       filename: req.file.originalname.slice(0, -4)
     });
     //
     // //pipe multer's temp file /uploads/filename into the stream we created above. On end deletes the temporary file.
-    fs.createReadStream("./uploads/" + req.file.filename)
+    /*fs.createReadStream("./uploads/" + req.file.filename)
       .on("end", function(){fs.unlink("./uploads/"+ req.file.filename, function(err){res.send("success")})})
+        .on("err", function(){res.send("Error uploading image")})
+          .pipe(writestream);*/
+
+      fs.createReadStream("./uploads/" + req.file.filename)
+      .on("end", function(){/*fs.unlink("./uploads/"+ req.file.filename, function(err){res.send("success")})*/res.send("success")})
         .on("err", function(){res.send("Error uploading image")})
           .pipe(writestream);
   });
